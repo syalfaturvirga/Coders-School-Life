79958670819172201b9015576a37dabef0d4ca56
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2022-11-20 14:05:49 +0700

    Camera dynamic movement and rolling door

---
+++ Assets/Character/Scripts/FollowPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    private Vector3 locationOffset = new Vector3(0, 1, -2);
+    private Vector3 rotationOffset = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // A very fixed update
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + target.rotation * locationOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        Quaternion desiredrotation = target.rotation * Quaternion.Euler(rotationOffset);
+        Quaternion smoothedrotation = Quaternion.Lerp(transform.rotation, desiredrotation, smoothSpeed);
+        transform.rotation = smoothedrotation;
+    }
+}
